0f60e022eb0bbbcfd7276e93285ea6115a347955
rodfer0x80 <rodfer0x80@proton.me>
2023-03-03 13:11:20 +0000

    [*] goat

--- vision/__main__.py
+++ vision/__main__.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+
+
+import sys
+import os
+
+
 # vision opencv module rpc connected to interface
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
