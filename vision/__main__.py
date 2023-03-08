2e74784484bb9c829dd5af3f24a217b5d55dabe8
rodfer0x80 <rodfer0x80@proton.me>
2023-03-08 19:10:25 +0000

    [*] rebuilding vision

--- vision/__main__.py
+++ vision/__main__.py
@@ -1,62 +1,38 @@
 #!/usr/bin/env python3
 
-
-import sys
 import os
-import time
+import sys
 
+from lib.UHR import *
 
-import cv2
-import mediapipe as mp
-# import numpy as np
+# https://colab.research.google.com/github/googlesamples/mediapipe/blob/main/examples/gesture_recognizer/python/gesture_recognizer.ipynb#scrollTo=Iy4r2_ePylIa
+#
+class MASVision:
+    def __init__(self):
+        config = {
+            "name": "MASVision",
+            "con": {"host": "127.0.0.1", "port": 5555,},
+            "data": [
+                {"name": "GESTURE", "gesture": "ALL", "trigger": "self.toggle_test"},
+            ],
+        }
 
-from lib.logger import Logger
+        self.uhr = UniversalHandRecognition(config)
 
-
-VISION_LOG = "vision_debug.log"
-
-def motionTracker(DEBUG=False):
-    logger = Logger(VISION_LOG)
+    def toggle_test(self):
+        print("test passed")
+        return 0
     
-    cap = cv2.VideoCapture(0)
-    mpHands = mp.solutions.hands
-    hands = mpHands.Hands()
+    def bootstrap(self):
+        try:
+            self.uhr.loopForever()
+        except KeyboardInterrupt:
+            return 0
+        return 1
 
-    #mpDraw = mp.solutions.drawing_utils
-    #cTime = 0
-    #pTime = 0
-
-    while True:
-        success, img = cap.read()
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = hands.process(imgRGB)
-        if results.multi_hand_landmarks:
-            for handlms in results.multi_hand_landmarks:
-                for id, lm in enumerate(handlms.landmark):
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x*w), int(lm.y*h)
-                    logger.debug(f"{id}, {cx}, {cy}")
-                    #print(id, cx, cy)
-                    # if id == 5:
-                    # cv2.circle(img, (cx, cy), 15, (139, 0, 0), cv2.FILLED)
-
-                # Time and FPS Calculation
-                #mpDraw.draw_landmarks(
-                #    img, handlms, mpHands.HAND_CONNECTIONS)
-
-        #cTime = time.time()
-        #fps = 1/(cTime-pTime)
-        #pTime = cTime
-        #cv2.putText(img, str(int(fps)), (10, 70),
-        #            cv2.FONT_HERSHEY_SIMPLEX, 3, (139, 0, 0), 3)
-        #            #cv2.imshow("Image", img)
-        cv2.waitKey(1)
-
-
-# vision opencv module rpc connected to interface
 def main():
-    motionTracker()
-    return 0
+    mas_vision = MASVision()
+    return mas_vision.bootstrap()
 
 
 if __name__ == '__main__':
