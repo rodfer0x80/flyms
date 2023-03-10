7f0d8bd36a85c8c7154cfa718f5d96d5babd7817
rodfer0x80 <rodfer0x80@proton.me>
2023-03-10 03:14:20 +0000

    [*] g

--- vision/__main__.py
+++ vision/__main__.py
@@ -54,29 +54,6 @@ class Vision:
         self.uhmt = UniversalHandMotionTracker()
 
     def bootstrap(self):
-        #while True:
-            #ret, frame = cap.read()
-            # Convert image to RGB for Mediapipe
-            #image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # Detect hand landmarks
-            #results = hands.process(image)
-            #print(results.multi_hand_landmarks)
-            #hand_landmarks = results.multi_hand_landmarks[0]
-            #print(hand_landmarks)
-            # Get hand position
-            #curr_x = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x
-            #curr_y = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
-            #print(curr_x, curr_y)
-            # Print hand gesture to screen
-            #cv2.putText(frame, gesture, (50, 50),
-            #            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            #cv2.imshow('Hand gesture detection', frame)
-            # Exit program when 'q' is pressed
-            #if cv2.waitKey(1) & 0xFF == ord('q'):
-            #    break
-        # Release OpenCV camera and Mediapipe hands
-        #cap.release()
-
         # Initialize hand tracking
         mp_drawing = mp.solutions.drawing_utils
         mp_drawing_styles = mp.solutions.drawing_styles
